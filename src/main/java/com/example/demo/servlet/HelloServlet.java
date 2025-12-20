4750fda0ec9f9d74717fa0769101f37817f3dd87
bala-amypo <bala@amypo.in>
2025-12-20 11:51:16 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,5 +1,27 @@
 package com.example.demo.servlet;
 
-public class HelloServlet {
-    
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Simple Hello Servlet
+ * Used to demonstrate Servlet + Tomcat concepts
+ */
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/plain");
+
+        PrintWriter out = response.getWriter();
+        out.println("Hello from Smart Invoice Application Servlet!");
+    }
 }
