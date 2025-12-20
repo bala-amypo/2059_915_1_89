e60e856d28e58a35fa38ac5f04ffd043e67a6212
bala-amypo <bala@amypo.in>
2025-12-20 12:15:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Invoice.java
+++ src/main/java/com/example/demo/entity/Invoice.java
@@ -1,63 +1,6 @@
-package com.example.demo.model;
+\package com.example.demo.entity;
 
-import jakarta.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(
-    uniqueConstraints = @UniqueConstraint(columnNames = {"vendor_id", "invoiceNumber"})
-)
 public class Invoice {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String invoiceNumber;
     private Double amount;
-    private LocalDate invoiceDate;
-    private String description;
-    private LocalDateTime uploadedAt;
-
-    @ManyToOne
-    private Vendor vendor;
-
-    @ManyToOne
-    private User uploadedBy;
-
-    @ManyToOne
-    private Category category;
-
-    @PrePersist
-    public void prePersist() {
-        this.uploadedAt = LocalDateTime.now();
-    }
-
-    // Getters & Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getInvoiceNumber() { return invoiceNumber; }
-    public void setInvoiceNumber(String invoiceNumber) { this.invoiceNumber = invoiceNumber; }
-
-    public Double getAmount() { return amount; }
-    public void setAmount(Double amount) { this.amount = amount; }
-
-    public LocalDate getInvoiceDate() { return invoiceDate; }
-    public void setInvoiceDate(LocalDate invoiceDate) { this.invoiceDate = invoiceDate; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    public LocalDateTime getUploadedAt() { return uploadedAt; }
-
-    public Vendor getVendor() { return vendor; }
-    public void setVendor(Vendor vendor) { this.vendor = vendor; }
-
-    public User getUploadedBy() { return uploadedBy; }
-    public void setUploadedBy(User uploadedBy) { this.uploadedBy = uploadedBy; }
-
-    public Category getCategory() { return category; }
-    public void setCategory(Category category) { this.category = category; }
 }
