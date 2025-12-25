5ec4642651106a1b9c0f65e8ef4f054cb382b568
bala-amypo <bala@amypo.in>
2025-12-25 17:33:05 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
@@ -1,13 +1,21 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.model.*;
-import com.example.demo.repository.*;
+import com.example.demo.model.Invoice;
+import com.example.demo.model.User;
+import com.example.demo.model.Vendor;
+import com.example.demo.repository.CategorizationRuleRepository;
+import com.example.demo.repository.InvoiceRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.VendorRepository;
+import com.example.demo.service.InvoiceService;
 import com.example.demo.util.InvoiceCategorizationEngine;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class InvoiceServiceImpl {
+@Service
+public class InvoiceServiceImpl implements InvoiceService {
 
     private final InvoiceRepository invoiceRepo;
     private final UserRepository userRepo;
@@ -29,6 +37,12 @@ public class InvoiceServiceImpl {
         this.engine = engine;
     }
 
+    @Override
+    public List<Invoice> getAllInvoices() {
+        return invoiceRepo.findAll();
+    }
+
+    @Override
     public Invoice uploadInvoice(Long userId, Long vendorId, Invoice invoice) {
         User user = userRepo.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
@@ -43,14 +57,17 @@ public class InvoiceServiceImpl {
         return invoiceRepo.save(invoice);
     }
 
+    @Override
     public Invoice getInvoice(Long id) {
         return invoiceRepo.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Invoice not found"));
     }
 
+    @Override
     public List<Invoice> getInvoicesByUser(Long userId) {
         User user = userRepo.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
         return invoiceRepo.findByUploadedBy(user);
     }
 }
