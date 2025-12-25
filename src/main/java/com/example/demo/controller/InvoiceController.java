171e414f3e2e66a20ef16fdc9beb7fbdab253836
bala-amypo <bala@amypo.in>
2025-12-25 17:27:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/InvoiceController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Invoice;
+import com.example.demo.service.InvoiceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/invoices")
+public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping
+    public List<Invoice> getAllInvoices() {
+        return invoiceService.getAllInvoices();
+    }
+}
